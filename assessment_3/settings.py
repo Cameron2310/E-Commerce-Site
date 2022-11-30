8465f9823691ae2f3af37496c286085e4761c26d
Cameron2310 <wilke7566@gmail.com>
2022-11-30 05:12:41 -1000

    initial commit

--- assessment_3/settings.py
+++ assessment_3/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nsu!rhc09(z+9oh$igaaz$vj*95i+db!hry21+79439#d%f9-b'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
